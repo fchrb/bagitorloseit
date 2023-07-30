de916af086ce83af264a0b15de601fca1d2d5649
fchrb <federico.cherubini@protonmail.com>
2023-07-30 19:06:38 +0200

    modified getEvents.js

--- getEvents.js
+++ getEvents.js
@@ -1,10 +1,7 @@
+const fetch = require('node-fetch');
+
 exports.handler = async (event, context) => {
     const API_ENDPOINT = 'https://www.googleapis.com/calendar/v3/calendars/54cc3f6d61bba548f6de7ba85ff358d777f9a51743d7dbdccbd58e02e8d323ae@group.calendar.google.com/events?maxResults=3&key=' + process.env.API_KEY;
-
-    let fetch;
-    await import('node-fetch').then(nodeFetch => {
-        fetch = nodeFetch.default || nodeFetch;
-    });
 
     return fetch(API_ENDPOINT, { headers: { Accept: 'application/json' } })
         .then(response => response.json())
