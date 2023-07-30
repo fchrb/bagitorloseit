5d9447794278c2f26b71b888474016c0396b8bb0
fchrb <federico.cherubini@protonmail.com>
2023-07-30 17:45:25 +0200

    functions folder

---
+++ functions/getEvents.js
@@ -0,0 +1,13 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event, context) => {
+    const API_ENDPOINT = 'https://www.googleapis.com/calendar/v3/calendars/54cc3f6d61bba548f6de7ba85ff358d777f9a51743d7dbdccbd58e02e8d323ae@group.calendar.google.com/events?maxResults=3&key=' + process.env.API_KEY;
+
+    return fetch(API_ENDPOINT, { headers: { Accept: 'application/json' } })
+        .then(response => response.json())
+        .then(data => ({
+            statusCode: 200,
+            body: JSON.stringify({ events: data.items }),
+        }))
+        .catch(error => ({ statusCode: 422, body: String(error) }));
+};
