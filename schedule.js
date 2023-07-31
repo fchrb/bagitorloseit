1f34bd4302c0ea0f932b02de957727b9f4e533aa
fchrb <federico.cherubini@protonmail.com>
2023-07-31 14:48:55 +0200

    test

--- schedule.js
+++ schedule.js
@@ -2,9 +2,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
     fetch('events.json')
         .then(response => response.json())
         .then(events => {
-            var streamsInParis = events.map(event => new Date(event.start + "+02:00")); // adjust for Paris timezone
-
-            // Rest of your code goes here...
+            var streamsInParis = events.map(event => ({
+                start: new Date(event.start),
+                summary: event.summary
+            }));
 
             // List of timezones
             var timezones = [
@@ -21,17 +22,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 // Add more timezones as necessary
             ];
 
-            // Your YouTube channel URL
-            var youtubeUrl = "https://www.youtube.com/yourchannel";
-
-            
-
             // Sort the streams and keep only the next 2
             streamsInParis.sort(function (a, b) {
-                return a.getTime() - b.getTime();
+                return a.start.getTime() - b.start.getTime();
             });
-            streamsInParis = streamsInParis.slice(0, 2);
-
+            streamsInParis = streamsInParis.filter(event => event.start.getTime() > Date.now()).slice(0, 2);
 
             // Fill the timezone dropdown
             var select = document.getElementById('timeZoneSelect');
@@ -49,21 +44,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 scheduleDisplay.innerHTML = '';
 
                 for (var i in streamsInParis) {
-                    var streamTime = new Date(streamsInParis[i]);
+                    var streamTime = new Date(streamsInParis[i].start);
                     var formatter = new Intl.DateTimeFormat('en-US', { timeZone: timezone, month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' });
                     var div = document.createElement('div');
-                    div.innerHTML = formatter.format(streamTime);
-
-                    // Create a link to YouTube
-                    var link = document.createElement('a');
-                    link.href = youtubeUrl;
-                    link.innerHTML = "on YouTube";
-                    link.target = "_blank";
-                    link.className = "youtube-link";
-
-                    // Append the link to the div
-                    div.appendChild(document.createElement('br'));
-                    div.appendChild(link);
+                    div.innerHTML = formatter.format(streamTime) + " - " + streamsInParis[i].summary;
 
                     scheduleDisplay.appendChild(div);
                 }
