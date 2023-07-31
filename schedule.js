b8d5115cd5573197f93aa96c524937e6a9d74af8
fchrb <federico.cherubini@protonmail.com>
2023-07-31 22:20:26 +0200

    various

--- schedule.js
+++ schedule.js
@@ -46,23 +46,27 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 for (var i in streamsInParis) {
                     var streamTime = new Date(streamsInParis[i].start);
                     var formatter = new Intl.DateTimeFormat('en-US', { timeZone: timezone, month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' });
-
+                
                     // Create the new div and apply the event-container class
                     var div = document.createElement('div');
                     div.className = 'event-container';
-                    div.innerHTML = formatter.format(streamTime) + ' ' + timezone;
+
+                    // Create a span for the stream time and append it to the div
+                    var timeSpan = document.createElement('span');
+                    timeSpan.className = 'event-time';
+                    timeSpan.textContent = formatter.format(streamTime) + ' ' + timezone;
+                    div.appendChild(timeSpan);
 
                     // Create the summary link and append it to the div
                     var summaryLink = document.createElement('a');
                     summaryLink.href = 'https://www.youtube.com/channel/UClft42jk2lPHSPQv9z4XB9w';
                     summaryLink.textContent = streamsInParis[i].summary;
                     summaryLink.className = 'summary-link';  // Apply the summary-link class
-
-                    // Append the summary link to the div
                     div.appendChild(summaryLink);
 
                     // Append the div to the schedule display
                     scheduleDisplay.appendChild(div);
+
                 }
             }
 
